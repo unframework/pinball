09e33b8fbe2091262045493166eb3783d2c207d4
Nick Matantsev <nick.matantsev@gmail.com>
2018-07-20 18:19:25 -0400

    Clean up unused code

--- pinball.ino
+++ pinball.ino
@@ -47,34 +47,6 @@ float vec2cross(float a[], float b[]) {
 int tvCX;
 int tvCY;
 
-#define LEFT_ANGLE (3 * M_PI / 4)
-#define RIGHT_ANGLE (M_PI / 4)
-
-float leftNormal[] = { cos(LEFT_ANGLE), sin(LEFT_ANGLE) };
-float rightNormal[] = { cos(RIGHT_ANGLE), sin(RIGHT_ANGLE) };
-
-#define BUMPER_OUTER(block) { \
-  { float* bumperNormal = leftNormal; float bumperHalfWidth = 3 / bumperNormal[1]; int bumperNudge = 12; block } \
-  { float* bumperNormal = rightNormal; float bumperHalfWidth = 3 / bumperNormal[1]; int bumperNudge = -12; block } \
-}
-
-#define BUMPER_LOOP(block) for (int j = -1; j <= 1; j += 1) { \
-  float vOffset = j * 32; \
-  float vOffsetX = vOffset * bumperNormal[0]; \
-  float vOffsetY = vOffset * bumperNormal[1]; \
-  \
-  for (int i = 0; i < 4; i += 1) { \
-    float hOffset = -60 + (120 + bumperNudge + 30 * i + j * 15) % 120; \
-    \
-    float bumperOffset = hOffset * bumperNormal[0] + vOffsetY; \
-    float bumperMiddle = hOffset * bumperNormal[1] - vOffsetX; \
-    float bumperLeft = -bumperHalfWidth + bumperMiddle; \
-    float bumperRight = bumperHalfWidth + bumperMiddle; \
-    \
-    block \
-  } \
-}
-
 struct bumperCircle {
   float center[2];
   float radius;
@@ -83,25 +55,6 @@ struct bumperCircle {
 float leftWallNormal[] = { 1, 0 };
 float rightWallNormal[] = { -1, 0 };
 float bottomWallNormal[] = { 0, 1 };
-
-float applyBumper(float ballOffset, float ballOffset_d, float ballAcross, float ballAcross_d, float portion, float bumperOffset, float bumperLeft, float bumperRight) {
-  // bounce?
-  float bottomPos_rel = ballOffset - bumperOffset;
-
-  // see if our (portioned) delta will "eat away" at any distance we have left
-  if (ballOffset_d > 0 && bottomPos_rel >= -EPS && bottomPos_rel < portion * ballOffset_d) {
-    float subPortion = bottomPos_rel / ballOffset_d;
-    float alongPos = ballAcross + subPortion * ballAcross_d;
-
-    // check against ends
-    if (alongPos > bumperLeft && alongPos < bumperRight) {
-      // return portion of travel
-      return subPortion;
-    }
-  }
-
-  return portion;
-}
 
 // inspired by stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm
 float applyBumperCircle(float ball[], float ball_d[], float portion, struct bumperCircle *circle) {
@@ -190,22 +143,6 @@ void physicsStep(float ball[], float ball_d[]) {
     //   closestBumperNormal = bottomWallNormal;
     // }
 
-    // BUMPER_OUTER({
-    //   float ballOffset = vec2dot(ball, bumperNormal);
-    //   float ballOffset_d = -vec2dot(ball_d, bumperNormal);
-    //   float ballAcross = vec2cross(bumperNormal, ball);
-    //   float ballAcross_d = vec2cross(bumperNormal, ball_d);
-
-    //   BUMPER_LOOP({
-    //     float portion = applyBumper(ballOffset, ballOffset_d, ballAcross, ballAcross_d, travelPortion, bumperOffset, bumperLeft, bumperRight);
-
-    //     if (portion < closestPortion) {
-    //       closestPortion = portion;
-    //       closestBumperNormal = bumperNormal;
-    //     }
-    //   })
-    // })
-
     bumperCircle mod1BumperCircle = { { 10 * floor(ball[0] / 10) + 5, 10 * floor(ball[1] / 10) + 5 }, 2 };
     bumperCircle mod2BumperCircle = { { 10 * floor((ball[0] - 5) / 10) + 10, 10 * floor((ball[1] - 5) / 10) + 10 }, 2 };
 
@@ -235,7 +172,6 @@ void physicsStep(float ball[], float ball_d[]) {
       travelPortion -= 0.05;
     }
 
-    // apply bumper restitution
     // apply bumper restitution (circles are done last, so they are checked first)
     if (closestBumperCircle != 0) {
       float normal[2];
@@ -276,7 +212,6 @@ void setup() {
   randomSeed(analogRead(0));
 
   TV.clear_screen();
-  // drawBumpers();
 
   // initial display for inverted draw to work
   for (struct ball_movement *ball = balls; ball < (struct ball_movement *)(&balls + 1); ball += 1) {
@@ -305,25 +240,4 @@ void drawBall(float ball[]) {
   // TV.draw_line(s_ball[0], s_ball[1] + 2, s_ball[0] + 2, s_ball[1], WHITE);
   // TV.draw_line(s_ball[0] + 2, s_ball[1], s_ball[0], s_ball[1] - 2, WHITE);
   // TV.draw_line(s_ball[0], s_ball[1] - 2, s_ball[0] - 2, s_ball[1], WHITE);
-}
-
-void drawBumper(int hOffset, int vOffset, float bumperNormal[], float bumperOffset, float bumperLeft, float bumperRight) {
-  int p1[2], p2[2];
-
-  // super hacky but more precise display for 45-degree line
-  int amt = bumperNormal[0] > 0 ? -3 : 3;
-  p1[0] = hOffset - 3;
-  p1[1] = vOffset - amt;
-  p2[0] = hOffset + 3;
-  p2[1] = vOffset + amt;
-
-  TV.draw_line(tvCX + p1[0], tvCY - p1[1], tvCX + p2[0], tvCY - p2[1], INVERT);
-}
-
-void drawBumpers() {
-  BUMPER_OUTER({
-    BUMPER_LOOP({
-      drawBumper(hOffset, vOffset, bumperNormal, bumperOffset, bumperLeft, bumperRight);
-    });
-  })
 }
